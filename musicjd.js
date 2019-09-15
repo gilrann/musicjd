95a053b2995c4b6e07ffe5c5efe5b36be5ce03f6
gilrann <gilrann@users.github.com>
2019-09-15 15:36:31 +0700

    Initial commit

---
+++ musicjd.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.history__slides').slick({
+  centerMode: true,
+  centerPadding: '4rem',
+  slidesToShow: 1,
+  arrows: false,
+  initialSlide: 2,
+  infinite: true
+  });
+});
