1d5c9c8af03fbd1bbbf6cf94c7e4e579f5144419
gilrann <gilrann@users.github.com>
2019-10-14 10:27:49 +0700

    112

--- musicjd.js
+++ musicjd.js
@@ -1,10 +1,37 @@
-  $(document).ready(function(){
-    $('.history__slides').slick({
-    centerMode: true,
-    centerPadding: '4rem',
-    slidesToShow: 1,
-    arrows: false,
-    initialSlide: 2,
-    infinite: true
-    });
-  });
+$('.history__slides').slick({
+ slidesToShow: 1,
+ slidesToScroll: 1,
+ arrows: false,
+ fade: false,
+ asNavFor: '.history__nav'
+});
+
+$('.history__nav').slick({
+ slidesToShow: 5,
+ slidesToScroll: 1,
+ asNavFor: '.history__slides',
+ centerMode: true,
+ infinite: true,
+ focusOnSelect: true,
+ responsive: [
+	    {
+	      breakpoint: 1440,
+	      settings: {
+	        slidesToShow: 3,
+          centerMode: true,
+          infinite: false
+	                 }
+	    },
+
+	    {
+	      breakpoint: 990,
+	      settings: {
+	        slidesToShow: 1,
+          centerMode: true,
+          infinite: false
+	                 }
+	    }
+
+    ]
+
+});
