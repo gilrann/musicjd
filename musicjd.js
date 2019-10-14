70a4ae89ef80d78f6f26eb6351a8051095aeaeda
gilrann <gilrann@users.github.com>
2019-10-14 10:39:45 +0700

    113

--- musicjd.js
+++ musicjd.js
@@ -1,3 +1,19 @@
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+
 $('.history__slides').slick({
  slidesToShow: 1,
  slidesToScroll: 1,
